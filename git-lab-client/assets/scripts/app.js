48e4263d7e65add27d3c2e190d6b9899ab577344
shkherad <shkherad@bu.edu>
2016-06-30 11:12:15 -0400

    folder creations
    folder creations

---
+++ git-lab-client/assets/scripts/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const app = {
+  host: 'https://wdi-library.herokuapp.com/books',
+};
+
+module.exports = app;
+
+
+// https://clothespin.herokuapp.com
--- git-lab-client/assets/scripts/index.js
+++ git-lab-client/assets/scripts/index.js
@@ -1,7 +1,16 @@
+
 'use strict';
 
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
 
 // use require without a reference to ensure a file is bundled
+
+const bookEvents = require('./book/book_events.js');
 require('./example');
+
+$(() => {
+  $('head').append('<link rel="stylesheet" href="./assets/styles/style.css">');
+  $('head').append("<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>");
+  bookEvents.addHandlers();
+});
